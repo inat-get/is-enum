557d0cc38dbbeb068af5782e5f1a53ec6c0034ed
Иван Шихалев <shikhalev@gmail.com>
2026-03-05 12:42:50 +0500

    bump version

--- lib/is-enum/info.rb
+++ lib/is-enum/info.rb
@@ -8,7 +8,7 @@ end
 module IS::Enum::Info
 
   NAME = 'is-enum'
-  VERSION = '0.8.8.6'
+  VERSION = '0.8.10'
   SUMMARY = 'Enum types for Ruby'
   AUTHOR  = 'Ivan Shikhalev'
   HOMEPAGE = 'https://github.com/inat-get/is-enum'
